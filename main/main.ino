b3538de5d1876bba5fdc334bd0f6736e12c5b26c
Benjamin Straub <bstraub@uc.cl>
2018-06-12 11:05:26 -0400

    reset_pin and round counter are added, data and selecct_region are fixed and movement od the motor on each stage is impemented

--- main/main.ino
+++ main/main.ino
@@ -22,6 +22,8 @@ int left_button = 40;
 int center_butotn = 38;
 int right_button = 36;
 
+int reset_pin = 53;
+
 // Instances of stepper and lcd
 int STEPS = 200; // 360º/1.8º
 Stepper motor(STEPS, stepper_1, stepper_2, stepper_3, stepper_4);
@@ -47,23 +49,24 @@ int region_selection_pos = 0;
 bool region_selected = false;
 bool data_converted = false;
 bool first_position = false;
+int round_counter = 0;
 
 // Data
-long region_1[8] = {5256, 4425, 2511, 2044, 1812, 1594, 1444, 37};
-long region_2[8] = {9029, 7862, 5214, 4247, 3594, 3040, 2751, 57};
-long region_3[8] = {4102, 3569, 2287, 1747, 1567, 1352, 1207, 21};
-long region_4[8] = {12093, 10342, 5468, 4789, 3889, 3230, 2913, 77};
-long region_5[8] = {31470, 27451, 15694, 13597, 11172, 9554, 7319, 149};
-long region_6[8] = {14390, 12347, 6568, 5776, 4700, 3638, 2621, 227};
-long region_7[8] = {17181, 15040, 8159, 7044, 5379, 4370, 3884, 148};
-long region_8[8] = {35749, 32193, 19573, 16437, 13101, 10563, 8658, 90};
-long region_9[8] = {15638, 13181, 7605, 6445, 5480, 4438, 3837, 77};
-long region_10[8] = {12862, 10870, 5721, 4887, 3962, 3310, 2923, 97};
-long region_11[8] = {1991, 1636, 783, 679, 552, 474, 358, 6};
-long region_12[8] = {2821, 2439, 1530, 1176, 1021, 906, 789, 26};
-long region_13[8] = {115149, 100861, 55189, 49552, 39689, 31311, 16194, 3664};
-long region_14[8] = {6672, 5696, 2959, 2548, 1899, 1531, 1436, 21};
-long region_15[8] = {4200, 3741, 2328, 1836, 1604, 1346, 1293, 12};
+long region_1[8] = {100, 84, 48, 39, 34, 30, 27, 1};
+long region_2[8] = {100, 87, 58, 47, 40, 34, 30, 1};
+long region_3[8] = {100, 87, 56, 43, 38, 33, 29, 1};
+long region_4[8] = {100, 86, 45, 40, 32, 27, 24, 1};
+long region_5[8] = {100, 87, 50, 43, 36, 30, 23, 0};
+long region_6[8] = {100, 86, 46, 40, 33, 25, 18, 2};
+long region_7[8] = {100, 88, 47, 41, 31, 25, 23, 1};
+long region_8[8] = {100, 90, 55, 46, 37, 30, 24, 0};
+long region_9[8] = {100, 84, 49, 41, 35, 28, 25, 0};
+long region_10[8] = {100, 85, 44, 38, 31, 26, 23, 1};
+long region_11[8] = {100, 82, 39, 34, 28, 24, 18, 0};
+long region_12[8] = {100, 86, 54, 42, 36, 32, 28, 1};
+long region_13[8] = {100, 88, 48, 43, 34, 27, 14, 3};
+long region_14[8] = {100, 85, 44, 38, 28, 23, 22, 0};
+long region_15[8] = {100, 89,	55,	44,	38, 32,	31,	0};
 
 // function that returns steps to move from x1 to x2
 int calculate_step(int x1, int x2){
@@ -101,39 +104,79 @@ void print_array(long my_array[]){
 // function that select the region based on the index of the array regions wich ocntain the names
 void select_region(int index){
   if (index == 0){
-    selected_region[8] = {region_15};
+    for (int i = 0; i < 8; i++){
+        selected_region[i] = region_15[i];
+    }
   } else if (index == 1){
-      selected_region[8] = {region_1};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_1[i];
+    }
   } else if (index == 2){
-      selected_region[8] = {region_2};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_2[i];
+    }
   } else if (index == 3){
-      selected_region[8] = {region_3};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_3[i];
+    }
   } else if (index == 4){
-      selected_region[8] = {region_4};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_4[i];
+    }
   } else if (index == 5){
-      selected_region[8] = {region_5};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_5[i];
+    }
   } else if (index == 6){
-      selected_region[8] = {region_13};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_13[i];
+    }
   } else if (index == 7){
-      selected_region[8] = {region_6};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_6[i];
+    }
   } else if (index == 8){
-      selected_region[8] = {region_7};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_7[i];
+    }
   } else if (index == 9){
-      selected_region[8] = {region_8};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_8[i];
+    }
   } else if (index == 10){
-      selected_region[8] = {region_9};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_9[i];
+    }
   } else if (index == 11){
-      selected_region[8] = {region_14};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_14[i];
+    }
   } else if (index == 12){
-      selected_region[8] = {region_10};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_10[i];
+    }
   } else if (index == 13){
-      selected_region[8] = {region_11};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_11[i];
+    }
   } else if (index == 14){
-      selected_region[8] = {region_12};
+      for (int i = 0; i < 8; i++){
+        selected_region[i] = region_12[i];
+    }
   }
+  print_array(int(select_region));
+  convert_data(selected_region);
+  print_array(int(select_region));
+}
+
+void convert_data(long data_array[8]){
+    for (int i = 0; i < 8; i++){
+        data_array[i] = map(data_array[i], 0 , 100, total_length, 0);
+    }
 }
 
 void setup() {
+  digitalWrite(reset_pin, HIGH);
   // Start of serial communication
   Serial.begin(9600);
 
@@ -149,35 +192,49 @@ void setup() {
   pinMode(left_button, INPUT);
   pinMode(center_butotn, INPUT);
   pinMode(right_button, INPUT);
+  pinMode(reset_pin, OUTPUT);
 
   // Setup of motor speed
   motor.setSpeed(20);
+  round_counter = 0;
 }
 
 void loop() {
   // User select region
   if (region_selected == false){
     two_liner2("Elige la region", regions[0]);
-  }
-  while (region_selected == false){
-    left_button_state = digitalRead(left_button);
-    center_button_state = digitalRead(center_butotn);
-    right_button_state = digitalRead(right_button);
+  
+    while (region_selected == false){
+      left_button_state = digitalRead(left_button);
+      center_button_state = digitalRead(center_butotn);
+      right_button_state = digitalRead(right_button);
 
-    if (right_button_state == HIGH && region_selection_pos < sizeof(regions)){
-      delay(500);
-      region_selection_pos = ++region_selection_pos;
-      two_liner2("Elige la region", regions[region_selection_pos]);
-    } else if (left_button_state == HIGH && region_selection_pos > 0){
-      delay(500);
-      region_selection_pos = --region_selection_pos;
-      two_liner2("Elige la region", regions[region_selection_pos]);
-    } else if (center_button_state == HIGH){
-      delay(500);
-      two_liner2("Elegiste", regions[region_selection_pos]);
-      select_region(region_selection_pos);
-      region_selected = true;
+      if (right_button_state == HIGH && region_selection_pos < sizeof(regions)){
+        delay(500);
+        region_selection_pos = ++region_selection_pos;
+        two_liner2("Elige la region", regions[region_selection_pos]);
+      } else if (left_button_state == HIGH && region_selection_pos > 0){
+        delay(500);
+        region_selection_pos = --region_selection_pos;
+        two_liner2("Elige la region", regions[region_selection_pos]);
+      } else if (center_button_state == HIGH){
+        delay(500);
+        two_liner2("Elegiste", regions[region_selection_pos]);
+        select_region(region_selection_pos);
+        region_selected = true;
+        round_counter = 0;
+      }
     }
   }
-
+  // loop for the eight steps
+  if (region_selected == true && round_counter < 7){
+      Serial.println("En el loop de los turnos!");
+      Serial.print("Loop "); Serial.println(round_counter);
+      round_counter = round_counter + 1;
+      int pos1 = selected_region[round_counter];
+      int pos2 = selected_region[round_counter + 1];
+      Serial.print(pos1); Serial.print(" "); Serial.println(pos2);
+      motor.step(calculate_step(pos1, pos2));
+      delay(1000);
+  }
 }
